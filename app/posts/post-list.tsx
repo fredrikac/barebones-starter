9d7dd661cf5271b8542f523800943f5a9759231f
Fredrika Carlsen <fredrika.carlsen@elastx.se>
2024-12-19 11:27:20 +0100

    very basic styling

--- app/posts/post-list.tsx
+++ app/posts/post-list.tsx
@@ -1,16 +1,28 @@
 import Link from "next/link";
+import "./post-list.css";
+import ".././styles.css";
 
 export default function PostList(props) {
   return (
     <>
       <h1>Posts</h1>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         {props.data.postConnection.edges.map((post) => (
-          <div key={post.node.id}>
-            <Link href={`/posts/${post.node._sys.filename}`}>
-              {post.node._sys.filename}
-            </Link>
-          </div>
+          <ul key={post.node.id}>
+            <li>
+              <Link
+                className="postLink"
+                href={`/posts/${post.node._sys.filename}`}
+              >
+                {post.node._sys.filename}
+              </Link>
+            </li>
+          </ul>
         ))}
       </div>
     </>
