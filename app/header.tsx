8e8da6d72be044a2eed493698a5acedc9b55c724
Fredrika Carlsen <fredrika.carlsen@elastx.se>
2025-01-17 10:47:55 +0100

    Update header layout and improve post sorting logic

--- app/header.tsx
+++ app/header.tsx
@@ -3,24 +3,26 @@ import "./globals.css";
 
 const Header = () => {
   return (
-    <header className="w-full z-40 h-32 sticky top-0 z-0">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-32 flex items-center justify-between">
-        <nav className="flex items-center gap-6">
-          <Link href="/" className="font-customFont text-3xl">
-            fredrikac.dev
-          </Link>
-          <span>
-            <Link className="text-lg" href="/about">
-              About
+    <div className="h-32">
+      <header className="w-full z-40 h-32 sticky top-0">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-32 flex items-center justify-between">
+          <nav className="flex items-center gap-6">
+            <Link href="/" className="font-customFont text-3xl">
+              fredrikac.dev
             </Link>
-            {" | "}
-            <Link className="text-lg" href="/posts">
-              Posts
-            </Link>
-          </span>
-        </nav>
-      </div>
-    </header>
+            <span>
+              <Link className="text-lg" href="/about">
+                About
+              </Link>
+              {" | "}
+              <Link className="text-lg" href="/posts">
+                Posts
+              </Link>
+            </span>
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 };
 
