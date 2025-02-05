28ad39fa6de4a141e0c00b7671651bf0ebfc0aeb
Fredrika Carlsen <fredrika.carlsen@elastx.se>
2025-02-05 09:41:53 +0100

    Refactor styles and update typography for improved readability and consistency

--- app/header.tsx
+++ app/header.tsx
@@ -6,20 +6,18 @@ const Header = () => {
     <div className="h-32">
       <header className="w-full z-40 h-32 sticky top-0">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-32 flex items-center justify-between">
-          <nav className="flex items-center gap-6">
-            <Link
-              href="/"
-              className="font-customFont text-3xl text-primary-300"
-            >
-              <span className="text-quaternary-200">fredrikac</span>.dev
+          <nav className="flex items-baseline gap-6 font-customFont">
+            <Link href="/" className="text-3xl text-primary-300 pl-4">
+              <span className="text-secondary-500">fredrikac</span>
+              .dev
             </Link>
             <span>
               <Link className="text-md" href="/about">
-                About
+                about
               </Link>
               {" | "}
               <Link className="text-md" href="/posts">
-                Posts
+                blog
               </Link>
             </span>
           </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,58 +10,58 @@ module.exports = {
       colors: {
         primary: {
           // Wool
-          50: '#e8e8e9',
+          50: '#f0f0f0',
           75: '#a1a0a6',
           100: '#7a7982',
           200: '#403f4c',
           300: '#191826',
           400: '#12111b',
         },
-        // Antler
+        // Accent
         secondary: {
           50: '#f9f7f5',
           75: '#e5ddd4',
           100: '#dacfc3',
           200: '#cabaa9',
           300: '#bfac97',
-          400: '#86786a',
-          500: '#75695c',
+          400: '#395353', // fence-400 green
+          500: '#082828',// fence-500 green
         },
-        // Nuthatch
-        tertiary: {
-          50: '#f4f1ef',
-          75: '#d0c4be',
-          100: '#bdaba3',
-          200: '#a0877c',
-          300: '#8d6e61',
-          400: '#634d44',
-          500: '#634d44',
-        },
-        // Chili
-        quaternary: {
-          50: '#fae9eb',
-          75: '#eba3ab',
-          100: '#e37d88',
-          200: '#d64555',
-          300: '#ce1f32',
-          400: '#901623',
-          500: '#7e131f',
-          600: '#054639' // green
-        },
-        // Loafer
-        quinary: {
-          50: '#fbfbf9',
-          75: '#f0efe6',
-          100: '#eae8db',
-          200: '#e0decc',
-          300: '#dad7c1',
-          400: '#999787',
-          500: '#858376',
-        },
+        // // Nuthatch
+        // tertiary: {
+        //   50: '#f4f1ef',
+        //   75: '#d0c4be',
+        //   100: '#bdaba3',
+        //   200: '#a0877c',
+        //   300: '#8d6e61',
+        //   400: '#634d44',
+        //   500: '#634d44',
+        // },
+        // // Chili
+        // quaternary: {
+        //   50: '#fae9eb',
+        //   75: '#eba3ab',
+        //   100: '#e37d88',
+        //   200: '#d64555',
+        //   300: '#ce1f32',
+        //   400: '#901623',
+        //   500: '#7e131f',
+        //   600: '#054639' // green
+        // },
+        // // Loafer
+        // quinary: {
+        //   50: '#fbfbf9',
+        //   75: '#f0efe6',
+        //   100: '#eae8db',
+        //   200: '#e0decc',
+        //   300: '#dad7c1',
+        //   400: '#999787',
+        //   500: '#858376',
+        // },
       },
       fontFamily: {
         customFont: ['"Mystic Sunlite"'],
-        'serif': ['Georgia', 'serif'],
+        serif: ['Georgia', 'serif'],
       },
     },
   variants: {
